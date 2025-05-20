eb7c02bdd0b1d8297a89dd76792083844ca744f8
Komrad1991 <likutindan@gmail.com>
2025-05-20 12:01:00 +0300

    Fixed inheritance

--- ASD_L10/graph.h
+++ ASD_L10/graph.h
@@ -1,47 +1,5 @@
 #pragma once
-#include <iostream>
 #include <vector>
-#include <queue>
-#include <climits>
 
 
-int primMST(const std::vector<std::vector<std::pair<int, int>>>& graph, int start)
-{
-    int n = graph.size();
-    std::vector<int> key(n, INT_MAX);
-    std::vector<bool> inMST(n, false);
-    std::priority_queue<std::pair<int, int>,
-        std::vector<std::pair<int, int>>,
-        std::greater<>> pq;
-
-    key[start] = 0;
-    pq.emplace(0, start);
-
-    int totalWeight = 0;
-
-    while (!pq.empty())
-    {
-        int u = pq.top().second;
-        int currentKey = pq.top().first;
-        pq.pop();
-
-        if (inMST[u]) continue;
-
-        inMST[u] = true;
-        totalWeight += currentKey;
-
-        for (const auto& edge : graph[u])
-        {
-            int v = edge.first;
-            int weight = edge.second;
-
-            if (!inMST[v] && weight < key[v])
-            {
-                key[v] = weight;
-                pq.emplace(key[v], v);
-            }
-        }
-    }
-
-    return totalWeight;
-}
+int primMST(const std::vector<std::vector<std::pair<int, int>>>& graph, int start);
--- ASD_L10/main.cpp
+++ ASD_L10/main.cpp
@@ -10,13 +10,13 @@ int main()
 {
     std::vector<std::vector<std::pair<int, int>>> graph(4);
 
-    graph[0].emplace_back(1, 1);
-    graph[0].emplace_back(2, 3);
-    graph[1].emplace_back(0, 1);
-    graph[1].emplace_back(2, 2);
-    graph[2].emplace_back(0, 3);
-    graph[2].emplace_back(1, 2);
-    graph[2].emplace_back(3, 4);
-    graph[3].emplace_back(2, 4);
-    primMST(graph, 0);
+    graph[0].push_back(std::make_pair(1, 1));
+    graph[0].push_back(std::make_pair(2, 3));
+    graph[1].push_back(std::make_pair(0, 1));
+    graph[1].push_back(std::make_pair(2, 2));
+    graph[2].push_back(std::make_pair(0, 3));
+    graph[2].push_back(std::make_pair(1, 2));
+    graph[2].push_back(std::make_pair(3, 4));
+    graph[3].push_back(std::make_pair(2, 4));
+    std::cout << primMST(graph, 0);
 }
